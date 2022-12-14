b403afc3eb928a0883eddc6ae283933bf1eed1ab
MarBozzz <mbozzuto@libero.it>
2022-12-14 18:16:35 +0100

    [added] row list

---
+++ index.php
@@ -0,0 +1,50 @@
+CARS
+
+id - INT - AUTO INCREMENT, NOT NULL, PRIMARY KEY
+marca - VARCHAR(30) - NOT NULL
+modello - VARCHAR(30) - NOT NULL
+omologazione euro - TINYINT - NULL
+cilindrata SMALLINT - NOT NULL
+potenza - VARCHAR(20) - NOT NULL
+cambio - VARCHAR(30) - NOT NULL
+alimentazione - VARCHAR(30) - NOT NULL
+year - YEAR - NOT NULL
+segmento - VARCHAR(30) - NULL
+nuova/usata  - CHAR(5) - NOT NULL
+numero di porte TINYINT - NOT NULL
+posti omologati TINYINT - NOT NULL
+trazione - VARCHAR(20) - NULL
+dimensioni - VARCHAR(30) - NULL
+peso a vuoto SMALLINT - NULL
+capacità serbatoio SMALLINT - NULL
+volume bagagliaio SMALLINT - NULL
+consumo urbano motore termico  - FLOAT(3,1) NULL
+consumo extraurbano motore termico- FLOAT(3,1) NULL 
+abs - TINYINT(1) - NULL
+esp - TINYINT(1) - NULL
+aria condizionata - TINYINT(1) - NULL
+controllo attivo mantenimento corsia - TINYINT(1) - NULL
+frenata automatica di emergenza - TINYINT(1) - NULL
+controllo automatico adattivo della velocità - TINYINT(1) - NULL
+avviso del cambio di corsia - TINYINT(1) - NULL
+android auto - TINYINT(1) - NULL
+apple carplay - TINYINT(1) - NULL
+navigatore satellitare - TINYINT(1) - NULL
+monitoraggio angolo cieco - TINYINT(1) - NULL
+sensori di parcheggio - TINYINT(1) - NULL
+vetri oscurati - TINYINT(1) - NULL
+sedili in pelle  - TINYINT(1) - NULL
+sedili riscaldati - TINYINT(1) - NULL
+gancio traino - TINYINT(1) - NULL
+hands-free - TINYINT(1) - NULL
+cerchi in lega - TINYINT(1) - NULL
+pneumatici montati - VARCHAR(50) - NULL
+colore - VARCHAR(30) - NOT NULL
+cruise control - TINYINT(1) - NULL
+airbags - VARCHAR(150) - NULL
+sistama audio - VARCHAR(30) - NULL
+prezzo - MEDIUMINT - NOT NULL
+finanziamento - VARCHAR() - NULL
+leasing - VARCHAR() - NULL
+noleggio lungo termine - VARCHAR() - NULL
+garanzia - VARCHAR() - NULL
